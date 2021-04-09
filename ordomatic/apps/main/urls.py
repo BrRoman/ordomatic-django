402963683946202257050a83d5078346aa4c0543
BrRoman <editeur@traditions-monastiques.com>
2021-04-09 15:45:39 +0200

    Home page.

---
+++ ordomatic/apps/main/urls.py
@@ -0,0 +1,10 @@
+""" apps/main/urls.py """
+
+from django.urls import path
+
+from . import views
+
+app_name = 'main'
+urlpatterns = [
+    path('', views.home, name='home'),
+]
--- ordomatic/apps/main/views.py
+++ ordomatic/apps/main/views.py
@@ -1,3 +1,8 @@
+""" apps/main/views.py """
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """ Home page of Ordomatic. """
+    return render(request, 'main/home.html')
