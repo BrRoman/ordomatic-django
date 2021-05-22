460a0123c4ab01f6aa450fd74084c73002c7abfb
BrRoman <editeur@traditions-monastiques.com>
2021-05-22 19:11:17 +0200

    Signup OK.

--- ordomatic/apps/accounts/views.py
+++ ordomatic/apps/accounts/views.py
@@ -1,14 +1,34 @@
 """ apps/accounts/views.py """
 
+from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 
 def signup(request):
     """ Signup view. """
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return HttpResponseRedirect(reverse('main:home'))
+
+    else:
+        form = UserCreationForm()
+
     return render(
         request,
         'accounts/signup.html',
+        {
+            'form': form,
+        },
     )
 
 
