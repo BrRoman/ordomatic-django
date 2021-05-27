e0b79285b64218d5385f0ca4073c5223123be063
BrRoman <editeur@traditions-monastiques.com>
2021-05-27 04:43:49 +0200

    Ordos: CRUD OK.

--- ordomatic/apps/ordos/models.py
+++ ordomatic/apps/ordos/models.py
@@ -1,10 +1,16 @@
 """ apps/ordos/models.py """
 
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Ordo(models.Model):
     """ Ordo model. """
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        blank=True,
+    )
     name = models.CharField(
         max_length=255,
     )
--- ordomatic/apps/ordos/views.py
+++ ordomatic/apps/ordos/views.py
@@ -1,49 +1,105 @@
 """apps/ordos/views.py """
 
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .forms import OrdoForm
+from .models import Ordo
 
 
+@login_required
 def ordos_list(request):
     """ List of ordos. """
+    ordos = Ordo.objects.filter(owner=request.user)
     return render(
         request,
         'ordos/list.html',
+        {
+            'ordos': ordos,
+        }
     )
 
 
+@login_required
 def ordo_create(request):
     """ Create an ordo. """
+    if request.method == 'POST':
+        form = OrdoForm(request.POST)
+        if form.is_valid():
+            ordo = form.save(commit=False)
+            ordo.owner = request.user
+            ordo.save()
+            return HttpResponseRedirect(reverse('ordos:ordos_list'))
+
+    else:
+        form = OrdoForm()
+
     return render(
         request,
         'ordos/form.html',
         {
-            'action': 'create',
-        },
+            'form': form,
+        }
     )
 
 
-def ordo_details(request):
+@login_required
+def ordo_details(request, **kwargs):
     """ Details of an ordo. """
+    ordo = get_object_or_404(Ordo, pk=kwargs['pk'])
     return render(
         request,
         'ordos/details.html',
+        {
+            'ordo': ordo,
+        }
     )
 
 
-def ordo_update(request):
+@login_required
+def ordo_update(request, **kwargs):
     """ Update an ordo. """
+    ordo = get_object_or_404(Ordo, pk=kwargs['pk'])
+
+    if request.method == 'POST':
+        form = OrdoForm(request.POST, instance=ordo)
+        if form.is_valid():
+            ordo = form.save(commit=False)
+            ordo.owner = request.user
+            ordo.save()
+            return HttpResponseRedirect(reverse('ordos:ordos_list'))
+
+    else:
+        form = OrdoForm(instance=ordo)
+
     return render(
         request,
         'ordos/form.html',
         {
-            'action': 'update',
-        },
+            'form': form,
+            'ordo': ordo,
+        }
     )
 
 
-def ordo_delete(request):
+@login_required
+def ordo_delete(request, **kwargs):
     """ Delete an ordo. """
+    ordo = get_object_or_404(Ordo, pk=kwargs['pk'])
+    if request.method == 'POST':
+        form = OrdoForm(request.POST, instance=ordo)
+        ordo.delete()
+        return HttpResponseRedirect(reverse('ordos:ordos_list'))
+
+    form = OrdoForm(instance=ordo)
+
     return render(
         request,
         'ordos/delete.html',
+        {
+            'form': form,
+            'ordo': ordo,
+        },
     )
