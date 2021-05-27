639c9ebfc1bca339dd948286710da8f855693b67
BrRoman <editeur@traditions-monastiques.com>
2021-05-27 21:29:26 +0200

    App docs: main volumes.

--- ordomatic/ordomatic/urls.py
+++ ordomatic/ordomatic/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('ordomatic/', include('apps.main.urls')),
+    path('ordomatic/docs/', include('apps.docs.urls')),
     path('ordomatic/accounts/', include('apps.accounts.urls')),
     path('ordomatic/calendars/', include('apps.calendars.urls')),
     path('ordomatic/days/', include('apps.days.urls')),
