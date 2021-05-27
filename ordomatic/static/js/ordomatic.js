b529fb72c6a7ab987aa115e7e71062b308e0285d
BrRoman <editeur@traditions-monastiques.com>
2021-05-27 21:32:04 +0200

    Ordomatic.js auto-formatted by HookyQR.beautify.

--- ordomatic/static/js/ordomatic.js
+++ ordomatic/static/js/ordomatic.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
-    $('#year').click(function(){
+    $('#year').click(function () {
         get_ordo_as_html();
     }).trigger('click');
 
-    $('#calendars').change(function(){
+    $('#calendars').change(function () {
         get_ordo_as_html();
     }).trigger('change');
 });
 
-function get_ordo_as_html(){
+function get_ordo_as_html() {
     const calendar = $('#calendars option:selected').val();
     const year = $('#year').val();
     console.log(calendar, year);
     $.get(
         '/ordomatic/get_ordo_as_html/' + calendar + '/' + year + '/',
-        function(back){
+        function (back) {
             $('#ordo_block').html(back);
         },
         'html',
