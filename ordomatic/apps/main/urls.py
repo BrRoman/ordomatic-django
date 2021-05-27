870d7d85258c4660533b2b43270932c497e681cd
BrRoman <editeur@traditions-monastiques.com>
2021-05-27 19:11:36 +0200

    Get_ordo_as_html: according to selected calendar.

--- ordomatic/apps/main/urls.py
+++ ordomatic/apps/main/urls.py
@@ -8,9 +8,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.home, name='home'),
     path('ordo/', views.ordo, name='ordo'),
-    path(
-        'get_ordo_as_html/<int:year>/',
-        views.get_ordo_as_html,
-        name='get_ordo_as_html',
-    ),
+    path('get_ordo_as_html/<int:calendar>/<int:year>/',
+         views.get_ordo_as_html, name='get_ordo_as_html'),
 ]
--- ordomatic/static/js/ordomatic.js
+++ ordomatic/static/js/ordomatic.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
     $('#year').click(function(){
-        const year = $(this).val();
-        $.get(
-            '/ordomatic/get_ordo_as_html/' + year,
-            function(back){
-                $('#ordo_block').html(back);
-            },
-            'html',
-        );
+        get_ordo_as_html();
     }).trigger('click');
+
+    $('#calendars').change(function(){
+        get_ordo_as_html();
+    }).trigger('change');
 });
+
+function get_ordo_as_html(){
+    const calendar = $('#calendars option:selected').val();
+    const year = $('#year').val();
+    console.log(calendar, year);
+    $.get(
+        '/ordomatic/get_ordo_as_html/' + calendar + '/' + year + '/',
+        function(back){
+            $('#ordo_block').html(back);
+        },
+        'html',
+    );
+}
